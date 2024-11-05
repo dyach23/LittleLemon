3943627b5b7f2d628fbcf5f66c000429416371c2
dyach <darwin2391998.dac@gmail.com>
2024-11-05 17:22:01 -0500

    first commit

---
+++ reservation/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
+    
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    number_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    
+    def __str__(self):
+        return f"Booking for {self.name} on {self.booking_date}"
