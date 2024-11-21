7321bd2487b355c5d5809df750d638e6c108c907
dyach <darwin2391998.dac@gmail.com>
2024-11-20 23:52:18 -0500

    adding test and authorization

--- reservation/urls.py
+++ reservation/urls.py
@@ -3,8 +3,8 @@ from .views import menuItemView, singleMenuItemView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('menu/', menuItemView.as_view()),
-    path('menu/<int:pk>', singleMenuItemView.as_view()),
+    path('menu/', menuItemView.as_view(), name='menu'),
+    path('menu/<int:pk>', singleMenuItemView.as_view(), name='menu-detail'),
     path('api-token-auth/', obtain_auth_token),
     
 ]
